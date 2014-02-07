90f2ffc3559454710278628572fddaa09ee023f9
benchwarmer <dschevyvette@gmail.com>
2014-02-06 22:54:51 -0600

    Working on collision detection

---
+++ Assets/Assets/Scripts/Enemy_script.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy_script : MonoBehaviour {
+
+	public float moveSpeed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Scoll object
+		transform.Translate (new Vector3 (moveSpeed * -1, 0, 0) * Time.deltaTime);
+
+
+	
+	}
+
+	void onTriggerEnter2D(Collider2D collider) {
+		Destroy (collider.gameObject);
+
+	}
+}
